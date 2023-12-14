108bba7e85203af5557d0bd518f18b775b7e696e
shabab <22752622+shababmoali@users.noreply.github.com>
2023-12-14 18:55:41 -0500

    feat: setup app for svelte frontend and fast api backend (#1)

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import json
+
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+@app.get("/")
+async def root():
+    # # get food data
+    with open("foods.json") as f:
+        data = json.load(f)
+    return data
