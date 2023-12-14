64e088a22be585d2f4c6672e8c81e2a29c5d82a4
Shabab <22752622+shababali@users.noreply.github.com>
2023-12-14 14:23:45 -0500

    setup frontend backend in monolith

--- app/main.py
+++ app/main.py
@@ -1,29 +1,24 @@
 import json
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
-# Jinja environment setup
-templates = Environment(
-    loader=FileSystemLoader("templates"),
-    autoescape=select_autoescape(['html', 'xml'])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
+
 @app.get("/")
-@app.post("/")
 async def root():
     # # get food data
     with open("foods.json") as f:
         data = json.load(f)
-
-    # return data
-    food_names_array = [e["name"] for category in data.values() for e in category]
-    #
-    template = templates.get_template("form_template.html")
-    html_content = template.render(food_names_array=food_names_array)  # You can pass context variables as arguments if needed
-    #
-    return HTMLResponse(content=html_content)
+    return data
